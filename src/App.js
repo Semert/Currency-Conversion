b6c080879e5c6488f85ad6de179d61dd53a45cc5
Semert <semert2121@gmail.com>
2020-12-07 20:15:57 +0300

    react-boostrap added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Alert from "react-bootstrap/Alert";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Alert dismissible variant="danger">
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <p>Change this and that and try again.</p>
+      </Alert>
     </div>
   );
 }
